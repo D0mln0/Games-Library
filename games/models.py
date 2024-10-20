055803edcb73c168c34b0b78d74f5d6f3278de09
Vova <v.kampanchenko@gmail.com>
2024-10-20 16:41:10 +0300

    Create main page

--- games/models.py
+++ games/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Publisher(models.Model):
     name = models.CharField(max_length=255)
+    info = models.TextField(null=True, blank=True)
 
 
 class Game(models.Model):
---
+++ games/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from games.views import index
+
+app_name = "games"
+
+urlpatterns = [
+    path("", index, name="index"),
+]
